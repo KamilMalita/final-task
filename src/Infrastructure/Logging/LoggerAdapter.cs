8df8edeecad5c7730e50d0a9c9f98b4e2151a104
Kamil Malita <36733743+KamilMalita@users.noreply.github.com>
2022-02-13 19:40:06 +0000

    Add files via upload

---
+++ src/Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.eShopWeb.Infrastructure.Logging;
+
+public class LoggerAdapter<T> : ApplicationCore.Interfaces.IAppLogger<T>
+{
+    private readonly Extensions.Logging.ILogger<T> _logger;
+    public LoggerAdapter(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<T>();
+    }
+
+    public void LogWarning(string message, params object[] args)
+    {
+        _logger.LogWarning(message, args);
+    }
+
+    public void LogInformation(string message, params object[] args)
+    {
+        _logger.LogInformation(message, args);
+    }
+}
