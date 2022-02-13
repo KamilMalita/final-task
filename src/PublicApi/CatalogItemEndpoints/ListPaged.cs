554efce05189a0e9a2c43b8818684b2764589b71
kamil malita <kamil.malita@gmail.com>
2022-02-13 19:48:41 +0000

    final impl

---
+++ src/PublicApi/CatalogItemEndpoints/ListPaged.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Microsoft.eShopWeb.PublicApi.CatalogItemEndpoints;
+
+public class ListPaged : EndpointBaseAsync
+    .WithRequest<ListPagedCatalogItemRequest>
+    .WithActionResult<ListPagedCatalogItemResponse>
+{
+    private readonly IRepository<CatalogItem> _itemRepository;
+    private readonly IUriComposer _uriComposer;
+    private readonly IMapper _mapper;
+    private readonly ILogger<ListPaged> _logger;
+
+   
+    public ListPaged(IRepository<CatalogItem> itemRepository,
+        IUriComposer uriComposer,
+        IMapper mapper,
+        ILogger<ListPaged> logger)
+    {
+        _logger = logger;
+        _itemRepository = itemRepository;
+        _uriComposer = uriComposer;
+        _mapper = mapper;
+    }
+
+    [HttpGet("api/catalog-items")]
+    [SwaggerOperation(
+        Summary = "List Catalog Items (paged)",
+        Description = "List Catalog Items (paged)",
+        OperationId = "catalog-items.ListPaged",
+        Tags = new[] { "CatalogItemEndpoints" })
+    ]
+    public override async Task<ActionResult<ListPagedCatalogItemResponse>> HandleAsync([FromQuery] ListPagedCatalogItemRequest request, CancellationToken cancellationToken) 
+    {
+        _logger.LogError("Strange error1");
+        _logger.LogDebug("Strange error2");
+        _logger.LogInformation("Paged request start!");
+        var response = new ListPagedCatalogItemResponse(request.CorrelationId());
+
+        var filterSpec = new CatalogFilterSpecification(request.CatalogBrandId, request.CatalogTypeId);
+        int totalItems = await _itemRepository.CountAsync(filterSpec, cancellationToken);
+
+        var pagedSpec = new CatalogFilterPaginatedSpecification(
+            skip: request.PageIndex * request.PageSize,
+            take: request.PageSize,
+            brandId: request.CatalogBrandId,
+            typeId: request.CatalogTypeId);
+
+        var items = await _itemRepository.ListAsync(pagedSpec, cancellationToken);
+
+        response.CatalogItems.AddRange(items.Select(_mapper.Map<CatalogItemDto>));
+        foreach (CatalogItemDto item in response.CatalogItems)
+        {
+            item.PictureUri = _uriComposer.ComposePicUri(item.PictureUri);
+        }
+
+        if (request.PageSize > 0)
+        {
+            response.PageCount = int.Parse(Math.Ceiling((decimal)totalItems / request.PageSize).ToString());
+        }
+        else
+        {
+            response.PageCount = totalItems > 0 ? 1 : 0;
+        }
+        _logger.LogError("Number of returned list products: " + response.CatalogItems.Count);
+        //return StatusCode(500, new Exception("Error i tyle").StackTrace);
+        throw new Exception("Cannot move further");
+        //throw new NotImplementedException("Cannot move further");
+        //return BadRequest("Cannot move further");
+        //return Ok(response);
+    }
+}
---
+++ src/Web/Pages/Basket/Success.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.eShopWeb.Web.Interfaces;
+
+namespace Microsoft.eShopWeb.Web.Pages.Basket;
+
+[Authorize]
+public class SuccessModel : PageModel
+{
+    /*private readonly IBasketViewModelService _basketViewModelService;
+    public SuccessModel(IBasketViewModelService basketViewModelService)
+    {
+        _basketViewModelService = basketViewModelService;
+    }
+
+    public BasketViewModel BasketModel { get; set; } = new BasketViewModel();
+    public async Task OnGet()
+    {
+        BasketModel = await _basketViewModelService.CreateBasketForUser(GetOrSetBasketCookieAndUserName());
+    }
+
+    private string GetOrSetBasketCookieAndUserName()
+    {
+        string userName = null;
+
+        if (Request.HttpContext.User.Identity.IsAuthenticated)
+        {
+            return Request.HttpContext.User.Identity.Name;
+        }
+
+        if (Request.Cookies.ContainsKey(Constants.BASKET_COOKIENAME))
+        {
+            userName = Request.Cookies[Constants.BASKET_COOKIENAME];
+
+            if (!Request.HttpContext.User.Identity.IsAuthenticated)
+            {
+                if (!Guid.TryParse(userName, out var _))
+                {
+                    userName = null;
+                }
+            }
+        }
+        if (userName != null) return userName;
+
+        userName = Guid.NewGuid().ToString();
+        var cookieOptions = new CookieOptions { IsEssential = true };
+        cookieOptions.Expires = DateTime.Today.AddYears(10);
+        Response.Cookies.Append(Constants.BASKET_COOKIENAME, userName, cookieOptions);
+
+        return userName;
+    }*/
+}
